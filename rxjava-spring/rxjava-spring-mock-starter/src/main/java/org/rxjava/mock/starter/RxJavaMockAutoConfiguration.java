7654cd27d19755012094e1364596eaad603e7396
Happy <wugang@billbear.cn>
2019-07-03 11:51:43 +0800

    mock模块占坑

---
+++ rxjava-spring/rxjava-spring-mock-starter/src/main/java/org/rxjava/mock/starter/RxJavaMockAutoConfiguration.java
@@ -0,0 +1,10 @@
+package org.rxjava.mock.starter;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author happy 2019-07-03 11:37
+ */
+@Configuration
+public class RxJavaMockAutoConfiguration {
+}
