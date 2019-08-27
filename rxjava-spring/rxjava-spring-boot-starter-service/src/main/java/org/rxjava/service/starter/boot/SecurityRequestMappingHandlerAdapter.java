ae4eae7865e169149c73552ca2442b41640e7330
Happy <wugang@billbear.cn>
2019-08-28 01:20:43 +0800

    Apply: 测试

--- rxjava-spring/rxjava-spring-boot-starter-service/src/main/java/org/rxjava/service/starter/boot/SecurityRequestMappingHandlerAdapter.java
+++ rxjava-spring/rxjava-spring-boot-starter-service/src/main/java/org/rxjava/service/starter/boot/SecurityRequestMappingHandlerAdapter.java
@@ -7,6 +7,7 @@ import org.rxjava.common.core.annotation.Login;
 import org.rxjava.common.core.entity.LoginInfo;
 import org.rxjava.common.core.exception.UnauthorizedException;
 import org.rxjava.common.core.utils.JsonUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.PathContainer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.method.HandlerMethod;
--- rxjava-spring/rxjava-spring-mock-starter/src/main/java/org/rxjava/mock/starter/RxJavaMockAutoConfiguration.java
+++ rxjava-spring/rxjava-spring-mock-starter/src/main/java/org/rxjava/mock/starter/RxJavaMockAutoConfiguration.java
@@ -1,10 +1,36 @@
 package org.rxjava.mock.starter;
 
+import org.rxjava.common.core.utils.JsonUtils;
+import org.rxjava.mock.starter.loginInfo.MockLoginInfo;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * @author happy 2019-07-03 11:37
  */
 @Configuration
-public class RxJavaMockAutoConfiguration {
+public class RxJavaMockAutoConfiguration implements WebFilter {
+
+    private static final String LOGIN_INFO = "loginInfo";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange serverWebExchange, WebFilterChain webFilterChain) {
+        MockLoginInfo loginInfo = new MockLoginInfo();
+        loginInfo.setUserId("testUserId");
+
+        String loginInfoJson = null;
+        try {
+            loginInfoJson = URLEncoder.encode(JsonUtils.serialize(loginInfo), "utf8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        serverWebExchange.getRequest().getHeaders().add(LOGIN_INFO, loginInfoJson);
+        return webFilterChain.filter(serverWebExchange);
+    }
 }
