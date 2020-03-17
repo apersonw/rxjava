721367c666d6e5584aadd3b82f3ad0968cb6c7e2
武刚 <wugang@fenglincloud.com>
2020-03-17 10:15:22 +0800

    test

--- rxjava-example/rxjava-service-example/src/main/java/org/rxjava/service/example/person/GoodsController.java
+++ rxjava-example/rxjava-service-example/src/main/java/org/rxjava/service/example/person/GoodsController.java
@@ -1,7 +1,7 @@
 package org.rxjava.service.example.person;
 
+import lombok.extern.slf4j.Slf4j;
 import org.rxjava.common.core.annotation.Login;
-import org.rxjava.common.core.exception.ErrorMessageException;
 import org.rxjava.service.example.entity.Goods;
 import org.rxjava.service.example.entity.Group;
 import org.rxjava.service.example.entity.GroupGoods;
@@ -12,17 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @RestController
+@Slf4j
 public class GoodsController {
     @Autowired
     private GoodsService goodsService;
