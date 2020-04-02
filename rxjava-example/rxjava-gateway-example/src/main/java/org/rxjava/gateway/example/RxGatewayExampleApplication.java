e4b9d8c18105fad6b092e1979d7a018bbf519283
happy <wugang@fenglincloud.com>
2020-04-03 01:14:53 +0800

    使fu

--- rxjava-example/rxjava-gateway-example/src/main/java/org/rxjava/gateway/example/RxGatewayExampleApplication.java
+++ rxjava-example/rxjava-gateway-example/src/main/java/org/rxjava/gateway/example/RxGatewayExampleApplication.java
@@ -2,10 +2,17 @@ package org.rxjava.gateway.example;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.function.Predicate;
 
@@ -13,17 +20,9 @@ import java.util.function.Predicate;
  * @author happy 2019-05-10 18:06
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class RxGatewayExampleApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(RxGatewayExampleApplication.class).web(WebApplicationType.REACTIVE).run(args);
     }
-
-//    @Bean
-//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(p -> p.path("/**")
-//                        .filters(u -> u.rewritePath("/([^/]+)/(?<segment>.*)", "/${segment}"))
-//                        .uri("https://www.baidu.com"))
-//                .build();
-//    }
 }
