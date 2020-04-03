07b65a6c418cbe181ca1a58069ad51526ccda9fb
happy <wugang@fenglincloud.com>
2020-04-04 05:54:50 +0800

    集成测试整合中

--- rxjava-example/rxjava-service-example/src/test/java/org/rxjava/api/example/test/TestApplication.java
+++ rxjava-example/rxjava-service-example/src/test/java/org/rxjava/api/example/test/TestApplication.java
@@ -2,20 +2,27 @@ package org.rxjava.api.example.test;
 
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.rxjava.api.example.form.TestForm;
+import org.rxjava.api.example.model.TestModel;
+import org.rxjava.api.example.person.TestApi;
 import org.rxjava.service.example.RxServiceExampleApplication;
-import org.rxjava.service.example.form.TestForm;
 import org.rxjava.service.example.person.TestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,classes = RxServiceExampleApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {TestContext.class})
 public class TestApplication {
     @Autowired
-    private TestController testController;
+    private TestApi testApi;
+
     @Test
-    public void testSave(){
+    public void testSave() {
         TestForm form = new TestForm();
         form.setObjectId(new ObjectId());
-        testController.testPath("haha", form).block();
+        TestModel haha = testApi.testPath("haha", form).block();
+        System.out.println(haha);
     }
 }
--- rxjava-example/rxjava-service-example/src/test/java/org/rxjava/api/example/test/TestContext.java
+++ rxjava-example/rxjava-service-example/src/test/java/org/rxjava/api/example/test/TestContext.java
@@ -1,10 +1,28 @@
 package org.rxjava.api.example.test;
 
+import org.rxjava.api.example.person.TestApi;
+import org.rxjava.apikit.client.ClientAdapter;
+import org.rxjava.common.core.api.ReactiveHttpClientAdapter;
 import org.rxjava.service.starter.boot.RxJavaWebFluxConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@Import(RxJavaWebFluxConfigurer.class)
+//@Import(RxJavaWebFluxConfigurer.class)
 public class TestContext {
+
+    @Bean
+    public ClientAdapter clientAdapter() {
+        return ReactiveHttpClientAdapter.build(new DefaultFormattingConversionService(), WebClient.builder(), "rxjava-service-example");
+    }
+
+    @Bean
+    public TestApi testApi(ClientAdapter clientAdapter) {
+        TestApi testApi = new TestApi();
+        testApi.setClientAdapter(clientAdapter);
+        return testApi;
+    }
 }
