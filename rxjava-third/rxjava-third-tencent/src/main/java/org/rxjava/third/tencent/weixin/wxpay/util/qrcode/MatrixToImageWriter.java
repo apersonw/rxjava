9570279a8c8905c133757d6d881bb9f951147f18
happy <wugang@fenglincloud.com>
2020-06-25 00:33:21 +0800

    1、腾讯微信统一

---
+++ rxjava-third/rxjava-third-tencent/src/main/java/org/rxjava/third/tencent/weixin/wxpay/util/qrcode/MatrixToImageWriter.java
@@ -0,0 +1,45 @@
+package org.rxjava.third.tencent.weixin.wxpay.util.qrcode;
+
+import com.google.zxing.common.BitMatrix;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.imageio.ImageIO;
+
+public final class MatrixToImageWriter {
+    private static final int BLACK = -16777216;
+    private static final int WHITE = -1;
+
+    private MatrixToImageWriter() {
+    }
+
+    private static BufferedImage toBufferedImage(BitMatrix matrix) {
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+        BufferedImage image = new BufferedImage(width, height, 1);
+
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                image.setRGB(x, y, matrix.get(x, y) ? -16777216 : -1);
+            }
+        }
+
+        return image;
+    }
+
+    public static void writeToFile(BitMatrix matrix, String format, File file) throws IOException {
+        BufferedImage image = toBufferedImage(matrix);
+        if (!ImageIO.write(image, format, file)) {
+            throw new IOException("Could not write an image of format " + format + " to " + file);
+        }
+    }
+
+    public static void writeToStream(BitMatrix matrix, String format, OutputStream stream) throws IOException {
+        BufferedImage image = toBufferedImage(matrix);
+        if (!ImageIO.write(image, format, stream)) {
+            throw new IOException("Could not write an image of format " + format);
+        }
+    }
+}
+
