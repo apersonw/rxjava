feafbc0a4a06891864cbc26bf4c483023b16b9d1
happy <wugang@fenglincloud.com>
2020-07-19 02:25:29 +0800

    1、操作员调整为管理员

--- rxjava-spring/rxjava-spring-boot-core/src/main/java/org/rxjava/common/core/info/ManagerInfo.java
+++ rxjava-spring/rxjava-spring-boot-core/src/main/java/org/rxjava/common/core/info/ManagerInfo.java
@@ -4,16 +4,16 @@ import lombok.Data;
 import org.bson.types.ObjectId;
 
 /**
- * 操作员信息
+ * 管理员信息
  */
 @Data
-public class OperatorInfo implements LoginInfo {
+public class ManagerInfo implements LoginInfo {
     /**
-     * 操作员Id
+     * 管理员Id
      */
-    private ObjectId operatorId;
+    private ObjectId managerId;
     /**
-     * 操作员角色Id
+     * 管理员角色Id
      */
     private ObjectId roleId;
 }
