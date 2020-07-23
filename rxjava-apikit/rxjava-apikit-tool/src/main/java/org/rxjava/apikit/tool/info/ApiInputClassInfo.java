bd01689ae2b6f13938495142152598f75451f7ed
happy <wugang@fenglincloud.com>
2020-07-24 00:51:02 +0800

    1、增加参数类型需求判断

--- rxjava-apikit/rxjava-apikit-tool/src/main/java/org/rxjava/apikit/tool/info/ApiInputClassInfo.java
+++ rxjava-apikit/rxjava-apikit-tool/src/main/java/org/rxjava/apikit/tool/info/ApiInputClassInfo.java
@@ -18,6 +18,7 @@ public class ApiInputClassInfo extends FieldInfo {
     private boolean requestParam = false;
     private boolean jsonParam = false;
     private boolean requestPartParam = false;
+    private boolean required = true;
 
     public ApiInputClassInfo(String name, ClassTypeInfo classTypeInfo) {
         super(name, classTypeInfo);
